756370701f56fad60b55d23b8b67dce6ef769dee
XiNGRZ <chenxingyu92@gmail.com>
2017-02-23 12:03:21 +0800

    修复 onChangeValue 警告

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,12 +5,17 @@ import styles from '../styles/input';
 export default class Input extends Component {
 
   render() {
+    const {
+      onChangeValue,
+      ...others
+    } = this.props;
+
     return (
       <input
         className={styles.input}
         type="text"
-        onChange={({ target }) => this.props.onChangeValue(target.value)}
-        {...this.props}
+        onChange={({ target }) => onChangeValue(target.value)}
+        {...others}
       />
     );
   }
